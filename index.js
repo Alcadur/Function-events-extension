f78919e5e58578e849b9c20f2719324aafb25997
Piotr Chełmicki <Piotr Chemicki>
2016-10-20 23:06:23 +0200

    Add index.js file for npm

---
+++ index.js
@@ -0,0 +1,5 @@
+require('./src/Function._events');
+require('./src/Function.idGenerator');
+require('./src/Function.addEventListener');
+require('./src/Function.dispatch');
+require('./src/Function.removeEventListener');
